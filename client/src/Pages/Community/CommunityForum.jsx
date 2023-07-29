1d21d15abecb3a791bbf9fd19b114f6a1694bf67
yash suthar <ysuthar8055@gmail.com>
2023-07-30 01:55:09 +0530

    did nothing

--- client/src/Pages/Community/CommunityForum.jsx
+++ client/src/Pages/Community/CommunityForum.jsx
@@ -78,35 +78,40 @@ const CommunityForum = () => {
 
   return (
     <div className="community-forum p-4 bg-gray-900 text-white">
-        <h2 className="text-xl md:3xl font-bold mb-4 text-center my-10">Discussion</h2>
-        <div className=" flex justify-center">
-      <div className="threads-list  rounded-xl border-0  max-w-xl bg-gray-800 p-5">
-        {threads.map((thread) => (
-          <div
-            key={thread.id}
-            className="thread-item p-4 border-0 bg-gray-900 rounded-xl mb-4 overflow-hidden cursor-pointer hover:bg-gray-700"
-            onClick={() => handleThreadClick(thread)}
-          >
-            <h3 className="text-sm md:text-xl font-bold mb-2">{thread.title}</h3>
-            <p className="text-xs md:text-lg text-gray-400 mb-1">{thread.content}</p>
-            <p className="text-xs md:text-lg text-gray-300">Author: {thread.author}</p>
-          </div>
-        ))}
-      </div>
-      <div className="min-w-0 comments-section p-5 mx-5 border-0 rounded-xl bg-gray-800">
-        {currentThread && (
-          <React.Fragment>
-            <ThreadDetail thread={currentThread} />
-            <hr className="my-4" />
-            <CommentSection
-              comments={currentThread.comments}
-              onAddComment={handleAddComment}
-            />
-          </React.Fragment>
-        )}
-      </div>
-
+      <h2 className="text-3xl font-bold mb-4 text-center my-10">Discussion</h2>
+      <div className=" flex justify-center">
+        <div className="threads-list  rounded-xl border-0  max-w-xl bg-gray-800 p-5">
+          {threads.map((thread) => (
+            <div
+              key={thread.id}
+              className="thread-item p-4 border-0 bg-gray-900 rounded-xl mb-4 overflow-hidden cursor-pointer hover:bg-gray-700"
+              onClick={() => handleThreadClick(thread)}
+            >
+              <h3 className="text-sm md:text-xl font-bold mb-2">
+                {thread.title}
+              </h3>
+              <p className="text-xs md:text-lg text-gray-400 mb-1">
+                {thread.content}
+              </p>
+              <p className="text-xs md:text-lg text-gray-300">
+                Author: {thread.author}
+              </p>
+            </div>
+          ))}
         </div>
+        <div className="min-w-0 comments-section p-5 mx-5 border-0 rounded-xl bg-gray-800">
+          {currentThread && (
+            <React.Fragment>
+              <ThreadDetail thread={currentThread} />
+              <hr className="my-4" />
+              <CommentSection
+                comments={currentThread.comments}
+                onAddComment={handleAddComment}
+              />
+            </React.Fragment>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
