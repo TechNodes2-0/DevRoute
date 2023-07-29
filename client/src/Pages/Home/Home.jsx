97d36169eaf020ae77d06b6e8e5df77669ba98e1
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-07-29 22:22:29 +0530

    feat: feature section ✨

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,12 +3,14 @@
 import React from "react";
 import HeroSection from "../../Components/HeroSection";
 import Cards from "../../Components/Cards";
+import Feature from "../../Components/Feature";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <Cards />
+      <Feature />
     </div>
   );
 }
