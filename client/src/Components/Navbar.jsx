9bd610c789e18ee4b34a0ff82c96c21239ed2dfd
yash suthar <ysuthar8055@gmail.com>
2023-07-29 19:52:30 +0530

    navbar component completed with responsive to all screens

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
+import React from "react";
 
 function Navbar() {
   return (
-    <div className='border-0 rounded-lg bg-gray-800 font-bold'>
-        <div className='flex flex-row justify-between '>
-            <div>
+    <div className="text-sm md:text-lg border-0 rounded-2xl bg-gray-700 text-white mx-[1vw] my-2 cursor-default">
+      <div className=" py-3 px-[5vw] bg-gray-800 rounded-2xl">
+        <div className="flex flex-row justify-between ">
+          <div className="my-auto mx-1">
             <p>LOGO</p>
+          </div>
+          <div className="flex my-auto  mx-1 h-8">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-full px-2 border text-black border-gray-300 rounded-l-md focus:outline-none focus:ring focus:border-blue-300"
+              />
+              <button className="bg-blue-500 hover:bg-blue-600 text-white px-2  rounded-r-md">
+              🔍
+              </button>
+          </div>
+          <div className="my-auto mx-1">
+            <div className="font-semibold flex">
+              <button className="py-1 px-3 border-0 rounded-lg bg-blue-700 hover:bg-blue-800 mx-1">
+                LogIn
+              </button>
+              <button className="max-md:hidden py-1 px-3 border-0 rounded-lg bg-blue-700 hover:bg-blue-800 mx-1">
+                SignUp
+              </button>
             </div>
-            <div>
-                <div className='flex w-20'>
-                    <a>
-                        home
-                    </a>
-                    <a>
-                        search
-                    </a>
-                    <a>
-                        contact
-                    </a>
-                    <a>
-                        about
-                    </a>
-                </div>
-            </div>
-            <div>
-                <div>
-                    <button>
-                        LogIN
-                    </button>
-                    <button>
-                        SignUP
-                    </button>
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
+      <div className="p-1 text-sm">
+        <div className="mx-auto">
+          <div className="flex w-64 justify-around">
+            <a className="mx-1 font-semibold hover:text-blue-400">Home</a>
+            <a className="mx-1 font-semibold hover:text-blue-400">Search</a>
+            <a className="mx-1 font-semibold hover:text-blue-400">contact</a>
+            <a className="mx-1 font-semibold hover:text-blue-400">about</a>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
