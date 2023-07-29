65566d33b7beb96755c0981135988bd8a68f0cde
yash suthar <ysuthar8055@gmail.com>
2023-07-29 18:58:29 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Navbar from "./Components/Navbar";
 
 export default function App() {
   return (
     <div>
+      <Navbar/>
       <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </div>
   );
