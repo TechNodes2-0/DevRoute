5936031567683dffd03d159c5397b22463df49d7
Jayesh Yadav <107855172+JayeshYadav99@users.noreply.github.com>
2023-07-30 18:20:46 +0530

    Update Cards.jsx

--- client/src/Components/Cards.jsx
+++ client/src/Components/Cards.jsx
@@ -50,9 +50,9 @@ export default function Cards() {
               <div className="flex justify-between items-center">
                 <div>
                   <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-                    App Development
+                    Authentication
                   </h3>
-                  <p className="text-sm text-gray-500">26 job positions</p>
+                 
                 </div>
                 <div className="pl-3">
                   <svg
@@ -82,9 +82,9 @@ export default function Cards() {
               <div className="flex justify-between items-center">
                 <div>
                   <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-                    Arts & Entertainment
+                   Prop management
                   </h3>
-                  <p className="text-sm text-gray-500">9 job positions</p>
+                  {/* <p className="text-sm text-gray-500">9 job positions</p> */}
                 </div>
                 <div className="pl-3">
                   <svg
@@ -114,9 +114,9 @@ export default function Cards() {
               <div className="flex justify-between items-center">
                 <div>
                   <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-                    Accounting
+                    Context API
                   </h3>
-                  <p className="text-sm text-gray-500">11 job positions</p>
+                  {/* <p className="text-sm text-gray-500">11 job positions</p> */}
                 </div>
                 <div className="pl-3">
                   <svg
@@ -146,9 +146,9 @@ export default function Cards() {
               <div className="flex justify-between items-center">
                 <div>
                   <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-                    UI Designer
+                   CustomHooks
                   </h3>
-                  <p className="text-sm text-gray-500">37 job positions</p>
+                  {/* <p className="text-sm text-gray-500">37 job positions</p> */}
                 </div>
                 <div className="pl-3">
                   <svg
@@ -180,7 +180,7 @@ export default function Cards() {
                   <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
                     Apps
                   </h3>
-                  <p className="text-sm text-gray-500">2 job positions</p>
+           
                 </div>
                 <div className="pl-3">
                   <svg
@@ -202,69 +202,7 @@ export default function Cards() {
             </div>
           </a>
 
-          <a
-            className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800"
-            href="#"
-          >
-            <div className="p-4 md:p-5">
-              <div className="flex justify-between items-center">
-                <div>
-                  <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-                    Content Writer
-                  </h3>
-                  <p className="text-sm text-gray-500">10 job positions</p>
-                </div>
-                <div className="pl-3">
-                  <svg
-                    className="w-3.5 h-3.5 text-gray-500"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                  >
-                    <path
-                      d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
 
-          <a
-            className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800"
-            href="#"
-          >
-            <div className="p-4 md:p-5">
-              <div className="flex justify-between items-center">
-                <div>
-                  <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-                    Analytics
-                  </h3>
-                  <p className="text-sm text-gray-500">14 job positions</p>
-                </div>
-                <div className="pl-3">
-                  <svg
-                    className="w-3.5 h-3.5 text-gray-500"
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                  >
-                    <path
-                      d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
         </div>
       </div>
     </div>
